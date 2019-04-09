e50c7b424843a2067b34a904ae4605723ad996c3
hailey822@naver.com <hailey822@naver.com>
2019-04-09 16:49:24 -0400

    Test Git

---
+++ structure.py
@@ -0,0 +1,27 @@
+# object World
+#   attribute Species[]
+#   attribute Foods[]
+#
+# object DNA
+#   attribute genes
+#
+# object Species
+#   attribute DNA
+#   attribute location
+#   attributes predators[]
+#   attributes prey[]
+#   attribute age
+#   function move
+#   function eat
+#   function attack
+#   functino reproduce
+# 
+# object A inherits from Species....
+# object B inherits from Species....
+# object C inherits from Species....
+# ..... so on..... depending n design...... 
+#
+# object Food 
+#   attribute location
+#   attribute energy
+#   attribute life
